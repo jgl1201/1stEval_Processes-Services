74f722fd39f599dac208fe455ada3d78007da4d1
jgl1201 <114144390+jgl1201@users.noreply.github.com>
2023-10-19 08:56:20 +0200

    counting vowels

--- ProcesosServicios1_02_LanzadorProcesos/src/Ejemplo05_ContarVocales/Counter.java
+++ ProcesosServicios1_02_LanzadorProcesos/src/Ejemplo05_ContarVocales/Counter.java
@@ -2,41 +2,83 @@ package Ejemplo05_ContarVocales;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Counter {
 
-	// Dado un fichero de entrada y una letra contamos cuantas veces aparece dicha
-	// letra
+	// Metodo para un lector, lee ficheros
+	// Para leer un fichero
+	public static BufferedReader getBufferedReader(String nombreFichero) throws FileNotFoundException {
+
+		FileReader lector = new FileReader(nombreFichero);
+		BufferedReader bufferedReader = new BufferedReader(lector);
+		return bufferedReader;
+	}
+
+	// Metodo/Herramienta par escribir
+	// Para escribir en un fichero
+	public static PrintWriter getPrintWriter(String nombreFichero) throws IOException {
+
+		FileWriter fileWriter = new FileWriter(nombreFichero);
+		PrintWriter printWriter = new PrintWriter(fileWriter);
+		return printWriter;
+	}
+
+	// para obtener una lista dinamica(Strings) de las lineas de un fichero
+	public static ArrayList<String> getLineasFichero(String nombreFichero) throws IOException {
+
+		ArrayList<String> lineas = new ArrayList<String>();
+		// usamos el metodo de arriba
+		BufferedReader bfr = getBufferedReader(nombreFichero);
+
+		// Leemos líneas del fichero...
+		String linea = bfr.readLine();
+		while (linea != null) {
+			// Y las añadimos al array
+			lineas.add(linea);
+			linea = bfr.readLine();
+		}
+		// Fin del bucle que lee líneas
+		return lineas;
+	}
+
+	// Dado un fichero de entrada y una letra
+	// contamos cuantas veces aparece dicha letra
 	// y dejamos el recuento en un fichero de salida
 	public static void hacerRecuento(String fichEntrada, String letra, String fichSalida)
 			throws FileNotFoundException, IOException {
-		BufferedReader br = Utils.getBufferedReader(fichEntrada);
-		PrintWriter pw = Utils.getPrintWriter(fichSalida);
-		String lineaLeida = br.readLine();
 
-		int totalVocales = 0;
+		BufferedReader br = getBufferedReader(fichEntrada);
+
+		PrintWriter pw = getPrintWriter(fichSalida);
+
+		String linea = br.readLine();
+
+		int total = 0;
+
 		// Mientras no queden líneas....
-		while (lineaLeida != null) {
+		while (linea != null) {
 			// ...recorremos la linea...
-			for (int i = 0; i < lineaLeida.length(); i++) {
-				char letraLeida = lineaLeida.charAt(i);
-				char letraPasada = letra.charAt(0);
+			for (int i = 0; i < linea.length(); i++) {
+				char leida = linea.charAt(i);
+				char pasada = letra.charAt(0);
 				// incrementamos el contador
-				if (letraLeida == letraPasada) {
-					totalVocales++;
-				}
-				// fin del if
+				if (leida == pasada) {
+					total++;
+				} // fin del if
+			} // fin del for
 
-			}
-			// fin del for
 			// Pasamos a la siguiente linea
-			lineaLeida = br.readLine();
-		}
+			linea = br.readLine();
+		} // fin while
+
 		// Escribimos el total de vocales
 		// en el fichero de salida
-		pw.println(totalVocales);
+		pw.println(total);
 		pw.flush();
 		// Y cerramos los ficheros
 		pw.close();
@@ -45,11 +87,18 @@ public class Counter {
 	}
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		String nombreFicheroEntrada = args[0];
-		String letra = args[1];
-		String nombreFicheroResultado = args[2];
-		hacerRecuento(nombreFicheroEntrada, letra, nombreFicheroResultado);
-		// Fin del main
+		// Se probo con Scanner como por args
+
+		// Scanner sc = new Scanner(System.in);
+
+		System.out.println("intoduzca el nombre de fichero de entrada: ");
+		String fileIn = args[0];// sc.nextLine();
+		System.out.println("introduzca el caracter a buscar en dicho fichero: ");
+		String letter = args[1];// sc.nextLine();
+		System.out.println("introduzca el normbre de fichero de salida");
+		String fileOut = args[2];// sc.nextLine();
+
+		hacerRecuento(fileIn, letter, fileOut);
 	}
 
 }
--- ProcesosServicios1_02_LanzadorProcesos/src/Ejemplo05_ContarVocales/Launcher.java
+++ ProcesosServicios1_02_LanzadorProcesos/src/Ejemplo05_ContarVocales/Launcher.java
@@ -1,32 +1,112 @@
 package Ejemplo05_ContarVocales;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
 public class Launcher {
 
+	/*
+	 * 1. Recoger el primer parámetro (args[0]), que contendrá el fichero a
+	 * procesar. 2. Recoger el segundo parámetro, que contendrá el directorio de
+	 * “CLASSPATH” donde habrá que buscar la clase “UtilidadesFicheros”. ????? 3.
+	 * Recoger el tercer parámetro, que contendrá el directorio donde habrá que
+	 * buscar la clase “Contador”. 4. Una vez recogidos los parámetros, se lanzarán
+	 * VARIOS procesos utilizando la clase “ProcessBuilder”. 5. Los procesos se
+	 * ejecutarán y después recogeremos los resultados en UNOS FICHEROS con un dato
+	 * ENTERO
+	 */
+	@SuppressWarnings("resource")
+	public static int getResultadoFichero(String nombreFichero) {
+
+		int suma = 0;
+
+		try {
+			// cañeria al fichero
+			FileInputStream fichero = new FileInputStream(nombreFichero);
+			InputStreamReader fir = new InputStreamReader(fichero);
+			BufferedReader br = new BufferedReader(fir);
+
+			// SOLO LEE UNA LINEA
+			String linea = br.readLine();
+			suma = Integer.parseInt(linea);
+			return suma;
+		} catch (FileNotFoundException e) {
+			System.out.println("No se pudo abrir " + nombreFichero);
+		} catch (IOException e) {
+			System.out.println("No hay nada en " + nombreFichero);
+		}
+		return suma;
+	}
+
 	@SuppressWarnings("unused")
 	public static void main(String[] args) throws IOException, InterruptedException {
-		String ficheroEntrada = ""; // args[0];
-		//String classpathUtilidades = args[1];
-		String route = "Counter.java";
+		// Fichero song.txt
+		String entrada ="song.txt"; //args[0];
+		// Otra forma
+		//String entrada = args[0];
+
+		/*
+		 * String classpathUtilidades; classpathUtilidades = args[1];
+		 */
+
+		// C:\\ruta hasta contador.java
+		// String ruta ="\\src\\ejemplo5\\Contador.java";// args[1];
+		// Otra forma
+		String ruta = "\\src\\Ejemplo05_ContarVocales\\Counter.java";
+		// OJO MAYUSCULAS Y MINUSCULAS
 		String[] vocales = { "a", "e", "i", "o", "u" };
-		String classPath = System.getProperty("java.class.path");
-		System.out.println("Usando classpath:" + classPath);
-		
-		/* Se lanzan los procesos */
+
+		// "C:\Users\jogal\eclipse-workspace\ProcesosServicios1_02_LanzadorProcesos\src\Ejemplo05_ContarVocales\Counter.java"
+		// No me funciona ni a Pepe ni a otro
+		String classPath = System.getProperty("user.dir");
+
+		// revisar
+		//String classPath = System.getProperty("user.dir");
+		// borro lo comentado abajo
+		// classPath = ruta ;//+ ":" +
+		// classpathUtilidades;
+		System.out.println("Usando ruta:" + ruta);
+		System.out.println("Usando classpath: " + classPath);
+		System.out.println("classpath+ruta " + classPath + ruta);
+
+		// se lanzan los procesos
 		for (int i = 0; i < vocales.length; i++) {
 			String fichErrores = "Errores_" + vocales[i] + ".txt";
 			ProcessBuilder pb;
-			pb = new ProcessBuilder("java", "-cp", classPath, "nombre_paquete.ProcesadorFichero", ficheroEntrada,
-					vocales[i], vocales[i] + ".txt");
+			pb = new ProcessBuilder("java", classPath + ruta, entrada, vocales[i], vocales[i] + ".txt");
+			System.out.println(entrada);
 			// Si hay algún error, almacenarlo en un fichero
 			pb.redirectError(new File(fichErrores));
 			pb.start();
+			// fin del for
 		}
 
-		/* Esperamos un poco */
-		Thread.sleep(3000);
-	}
+		// COMO MOSTRARIAMOS los resultados de cada fichero
+		// COMO guardariamos dichos resultados en un fichero resultados totales
+		Thread.sleep(1000);
 
+		// total de vocales
+		String res = "RES.txt";
+		File f = new File("RES.txt");
+
+		PrintWriter pw = Counter.getPrintWriter(res);
+
+		for (int i = 0; i < vocales.length; i++) {
+			// abrir a.txt y mostrar a consola su resultado y demas vocales
+			// valueOf() para forzarlo a que transforme a String los Integers
+			String cad = "El numero de " + vocales[i] + " es: " + String.valueOf(getResultadoFichero(vocales[i] + ".txt"));
+
+			System.out.println(cad);
+
+			pw.append(cad + "\n");
+
+		}
+		pw.close();
+
+	}
 }
